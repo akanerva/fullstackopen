4be51879a32d10dd7b8da764d4fc63e4249c877b
Aleksi Kanerva <aleksi.kanerva@oulu.fi>
2024-09-11 17:13:05 +0300

    add 2.5

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,37 +1,4 @@
-const Course = ({ course }) => (
-  <div>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-  </div>
-);
-
-const Header = ({ name }) => <h2>{name}</h2>;
-
-const Content = ({ parts }) => {
-  const list = parts.map((part) => (
-    <Part key={part.id} name={part.name} exercises={part.exercises} />
-  ));
-  const total = parts
-    .map((part) => part.exercises)
-    .reduce((acc, current) => acc + current, 0);
-  return (
-    <>
-      <div>{list}</div>
-      <div>
-        <b>total of {total} exercises</b>
-      </div>
-    </>
-  );
-};
-
-const Part = ({ name, exercises }) => (
-  <>
-    <div>
-      {name} {exercises}
-    </div>
-    <br></br>
-  </>
-);
+import Course from "./Course";
 
 const App = () => {
   const courses = [
