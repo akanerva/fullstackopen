ec2c20fbe34af1e1d626559dbc45bf0085de3e1f
Aleksi Kanerva <aleksi.kanerva@oulu.fi>
2024-09-30 16:23:32 +0300

    add 3.6

--- osa3/index.js
+++ osa3/index.js
@@ -26,14 +26,14 @@ let persons = [
 ];
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  res.status(200).json(persons);
 });
 
 app.get("/api/persons/:id", (req, res) => {
   const id = req.params.id;
   const person = persons.find((person) => person.id === id);
   if (person) {
-    res.json(person);
+    res.status(200).json(person);
   } else {
     res.status(404).end();
   }
@@ -58,15 +58,23 @@ app.post("/api/persons", (req, res) => {
   console.log("body received: ", req.body);
   const name = req.body.name;
   const number = req.body.number;
+  if (!name) {
+    return res.status(400).json({ error: "name property missing" });
+  }
+  if (!number) {
+    return res.status(400).json({ error: "number property missing" });
+  }
+  if (persons.find((person) => person.name === name)) {
+    return res.status(400).json({ error: "name must be unique" });
+  }
   let id = "1";
   while (persons.find((person) => person.id === id)) {
     console.log(`id ${id} already exists. generating new one`);
     id = Math.floor(Math.random() * 10000);
   }
   const personToAdd = { id: id, name: name, number: number };
-  persons.concat({ id: id, name: name, number: number });
-  res.status(201);
-  res.json(personToAdd);
+  persons = persons.concat({ id: id, name: name, number: number });
+  res.status(201).json(personToAdd);
 });
 
 const PORT = 3001;
