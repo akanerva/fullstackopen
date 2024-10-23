c62ee818285eca58024b0b7476e1576779a79477
Aleksi Kanerva <aleksi.kanerva@oulu.fi>
2024-10-23 12:28:36 +0300

    add 3.12

---
+++ osa3/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+// import mongoose from "mongoose";
+
+if (process.argv.length < 3) {
+  console.log("password is required as an argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://aleksikanerva90:${password}@cluster0.jzg9s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv[3] && process.argv[4]) {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({})
+    .then((persons) => {
+      console.log("phonebook:");
+      persons.forEach((person) => {
+        console.log(`${person.name} ${person.number}`);
+      });
+    })
+    .then((res) => mongoose.connection.close());
+}
