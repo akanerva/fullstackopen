eab831daa299941b970e7dd3d9564da9537a798b
Aleksi Kanerva <aleksi.kanerva@oulu.fi>
2024-10-28 15:22:20 +0200

    add 4.4

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -8,3 +8,39 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
 });
+
+describe("totalLikes", () => {
+  const listWithOneBlog = [
+    {
+      id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+    },
+  ];
+  const listWithTwoBlogs = [
+    {
+      id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+    },
+    {
+      id: "5a422aa71b54a676234d17f4",
+      title: "Keep It Simple Stupid",
+      author: "Kelly Johnson",
+      url: "https://en.wikipedia.org/wiki/KISS_principle",
+      likes: 6,
+    },
+  ];
+  test("works with a list of one blog", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+  test("works with a list of two blogs", () => {
+    const result = listHelper.totalLikes(listWithTwoBlogs);
+    assert.strictEqual(result, 11);
+  });
+});
