f4769e0a449f3df794151caa01d4466fa1c8688d
Aleksi Kanerva <aleksi.kanerva90@gmail.com>
2024-12-06 05:58:50 +0200

    add 5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const collapsedBlog = () => (
+    <>
+      {blog.title} {blog.author}
+      <button onClick={() => setCollapsed(false)}>view</button>
+    </>
+  );
+
+  const fullBlog = () => (
+    <>
+      <div>
+        {blog.title} <button onClick={() => setCollapsed(true)}>hide</button>
+      </div>
+      <div>{blog.url}</div>
+      <div>
+        likes {blog.likes} <button>like</button>
+      </div>
+      <div>{blog.author}</div>
+    </>
+  );
+
+  return (
+    <div style={blogStyle}>
+      {collapsed && collapsedBlog()}
+      {!collapsed && fullBlog()}
+    </div>
+  );
+};
+export default Blog;
